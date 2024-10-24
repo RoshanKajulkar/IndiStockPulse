a2cbbd1e7cf29d428eee55ed3b5fea727910364e
Roshan Kajulkar <roshankajulkar@gmail.com>
2024-10-24 21:18:29 +0530

    adds home page

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes';
-import authRoutes from './routes/authRoute';
-import stockRoutes from './routes/stockRoutes';
-import { authMiddleware } from './middlewares/authMiddleware';
-import { rateLimiterMiddleware } from './middlewares/rateLimiterMiddleware';
+import express, { Request, Response } from "express";
+import path from "path";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+import authRoutes from "./routes/authRoute";
+import stockRoutes from "./routes/stockRoutes";
+import { authMiddleware } from "./middlewares/authMiddleware";
+import { rateLimiterMiddleware } from "./middlewares/rateLimiterMiddleware";
 
 dotenv.config();
 
@@ -14,10 +15,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/stocks', authMiddleware, rateLimiterMiddleware, stockRoutes);
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/stocks", authMiddleware, rateLimiterMiddleware, stockRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
